5bf4a06f1f4b1a9f19903b33a8effb352982dddb
minjae <minj219aa@gmail.com>
2021-12-27 23:53:17 +0900

    동영상 피드

--- Screens/FeedScreen.js
+++ Screens/FeedScreen.js
@@ -1,27 +1,34 @@
 import React, { useState } from 'react';
 import { Text, View, StyleSheet, FlatList, RefreshControl, Image, TouchableOpacity, Dimensions } from 'react-native';
 import { getStatusBarHeight } from 'react-native-status-bar-height';
-
-const renderItem = ({ item }) => {
-    const window = Dimensions.get('window')
-    return <TouchableOpacity style={{ flex: 1 }}>
-        <Image source={{ uri: item }}
-            style={{ flex: 1, height: window.width }}
-            resizeMode='cover'
-        />
-    </TouchableOpacity>
-};
+import Video from 'react-native-video'
+import { useIsFocused } from '@react-navigation/native';
 
 function FeedScreen(props) {
-    let feedData = [];
-    for (let i = 1; i < 11; i++) {
-        feedData.push(`https://patrasche.s3.ap-northeast-2.amazonaws.com/media/dog${i}.jpeg`)
-    }
+    const screenIsFocused = useIsFocused();
+    let feedData = ['https://patrasche.s3.ap-northeast-2.amazonaws.com/media/videos/dog3.mp4']
     const [refreshing, setRefreshing] = useState(false);
     const onRefresh = () => {
         setRefreshing(true);
         setTimeout(() => { setRefreshing(false) }, 3000)
     }
+
+    const renderItem = ({ item }) => {
+        const window = Dimensions.get('window')
+        return <TouchableOpacity style={{ flex: 1 }}>
+            <Video source={{ uri: item }}   // Can be a URL or a local file.
+                ref={(ref) => {
+                    this.player = ref
+                }}                                      // Store reference
+                onBuffer={this.onBuffer}                // Callback when remote video is buffering
+                onError={this.videoError}               // Callback when video cannot be loaded
+                style={{ height: window.height }}
+                repeat={true}
+                playInBackground={false}
+                paused={!screenIsFocused}
+            />
+        </TouchableOpacity>
+    };
     return (
         <View style={styles.container}>
             <FlatList
